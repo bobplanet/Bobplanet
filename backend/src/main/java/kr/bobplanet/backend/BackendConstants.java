51a46e7106ad281f71639e2e90fe2d34523a6ea9
Jin, Heonkyu <heonkyu.jin@gmail.com>
2015-10-15 23:22:24 +0900

    기밀정보 제거된 소스파일 복원

---
+++ backend/src/main/java/kr/bobplanet/backend/BackendConstants.java
@@ -0,0 +1,34 @@
+package kr.bobplanet.backend;
+
+/**
+ * Backend에서 사용되는 공통 상수값을 모아놓은 인터페이스.
+ *
+ * @author heonkyu.jin
+ * @version 15. 9. 29..
+ */
+public interface BackendConstants {
+	/**
+	 * 
+	 */
+    String GCM_API_KEY = "AIzaSyCgCD6Jbj7CqraZxDlOo7ZzaG7wVlN51MU";
+
+	/**
+	 *
+	 */
+    String API_OWNER = "backend.bobplanet.kr";
+
+	/**
+	 * 안드로이드 및 웹애플리케이션들의 클라이언트ID.
+	 * API접근권한 제어를 위해 사용됨 (이게 없으면 아무나 API를 호출할 수 있음)
+	 */
+    String CLIENTID_ANDROID_RELEASE = BuildConfig.CLIENTID_ANDROID_RELEASE;
+    String CLIENTID_ANDROID_DEV = BuildConfig.CLIENTID_ANDROID_DEV;
+    String CLIENTID_WEB = BuildConfig.CLIENTID_WEB;
+    String ANDROID_AUDIENCE = CLIENTID_WEB;
+
+	/**
+	 * OAuth 접근권한 제어시 사용할 email scope.
+	 */
+    String EMAIL_SCOPE =
+            "https://www.googleapis.com/auth/userinfo.email";
+}
